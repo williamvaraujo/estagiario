52cad172f8bc7e064f843ffd6c9addcd88184efb
Will Araujo <114265989+williamvaraujo@users.noreply.github.com>
2023-04-14 11:37:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #IMPORTAR AS BIBLIOTECAS NECESSÁRIAS
 
 from flask import Flask, request
+from gunicorn.config import Config
 import requests
 import gspread
 import json
@@ -20,11 +21,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 #TOKEN TELEGRAM #TELEGRAM_APY_TOKEN
 token_telegram = os.environ["TELEGRAM_API_KEY"]
 
+
 #TOKEN GOOGLE SHEETS API #ARQUIVO OCULTO NA RAIZ
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 with open("credenciais.json", mode="w") as fobj:
   fobj.write(GOOGLE_SHEETS_CREDENTIALS)
-id_da_planilha = "1kBqC3I-sW3paC3zdQ_9lGnAK64R38rmokeXJdrG7obU"   #ID_PLANILHA
+id_da_planilha = "1JMO_CCRtR7y2ntpYNZixc2Ma3e7VbtMj31zb7ymJc8c"   #ID_PLANILHA
 nome_da_pag = "NOME_PLANILHA"    #NOME_PLANILHA
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -37,17 +39,21 @@ cliente = gspread.authorize(gs_credenciais)
 planilha = cliente.open_by_key(id_da_planilha).sheet1
   
 #TOKEN_CHAT_GPT #TOKEN_CHATGPT
-token_chatgpt = "TOKEN_CHATGPT"
+token_chatgpt = os.environ["TOKEN_CHATGPT"]
+headers_chatgpt = {"Authorization": f"Bearer {token_chatgpt}", "content-type": "Application/json"}
+link_chatgpt = "https://api.openai.com/v1/chat/completions"
+id_modelo_chatgpt = "gpt-3.5-turbo"
 
 #CADASTRO DO E-MAIL
 # Configurar informações da conta
-email = "EMAIL" #email
-senha_email = "SENHA_EMAIL" #senha_email
+email = os.environ["EMAIL"] #email
+senha_email = os.environ["SENHA_EMAIL"] #senha_email
 #-----------------------------------------------------------------
 #################################################################
 
-#CONFIGURANDO ALGUMAS FUNÇÕES
+#FAZENDO A CONFIGURAÇÃO DOS CLIENTES DOS TOKENS
 
+#AQUI VAMOS CRIAR UMA FUNÇÃO PARA IDENTIFICAR E-MAILS NO STRING
 def parse_email_subject(text):
     if "@" in text:
         email, _, subject = text.partition(",")
@@ -64,22 +70,35 @@ def dividir_texto(texto):
     print(destinatario)
     print(assunto_do_email)
     return destinatario, assunto_do_email
- 
-offset = 0
     
+#AQUI, VAMOS FAZER A CONFIGURAÇÃO DE ACESSO AO MODELO IDEAL DE CHATPGT, INCLUSIVE, COM HEARDERS PARA ENVIAR UM POST JSON
 
+#----------------------------------------------------------------
+
+#Função para atualização do offset do link de endpoint da API
+#**OFFSET**
+#Identificador da primeira atualização a ser retornada. Deve ser maior em um do que o maior entre os identificadores de atualizações recebidas anteriormente.
+#Por padrão, as atualizações que começam com a atualização não confirmada mais antiga são retornadas. Uma atualização é considerada confirmada assim que getUpdates
+#é chamado com um offset maior que seu update_id . O deslocamento negativo pode ser especificado para recuperar atualizações a partir de -offset update a partir do
+#final da fila de atualizações. Todas as atualizações anteriores serão esquecidas.
+#OBSERVAÇÃO: Este offset não tem o mesmo significado do OFFSET presente nos dados da mensagem. Este offset representa o UPDATE_ID;
+#**Sempre buscaremos a última interação do usuário, por isso, o update_id e a mensagem serão as últimas do dicionário JSON. Serão [-1] para poderem ser os últimos.**
+
+#offset = 0
 ##############################################################################################################################
 ##############################################################################################################################
-#-------------------------------------------------------------------------------Configuração do FLASK
+
+
+
+
+
+#FUNÇÃO DE FUNCIONAMENTO DO BOT
+
 app = Flask(__name__)
-@app.route("/bot-estagiario", methods=["POST"])
-#-------------------------------------------------------------------------------Disparo da função
-def bot_estagiario():
+@app.route("/bot-das-pautas", methods=["POST"])
+def bot_das_pautas():
     #
-    primeira_mensagem = request.get_json()
-    
-    teste = requests.get(f"https://api.telegram.org/bot{token_telegram}/getUpdates?offset={offset + 1}").json()
-    print(teste)
+    primeira_mensagem = request.json
     ultima_mensagem = primeira_mensagem["message"]["text"]
     chat_id = primeira_mensagem["message"]["chat"]["id"]
     nome_usuario = primeira_mensagem["message"]["from"]["first_name"]  
@@ -87,55 +106,50 @@ def bot_estagiario():
     print(ultima_mensagem)
     print(chat_id)
     print(nome_usuario)
-    resposta = "Você não digitou uma mensagem válida. Por favor, verifique novamente as últimas instruções"
-#------------------------------------------------------------------------------ Comando /start
+    resposta = "Você não digitou uma mensagem válida. Tente /start novamente"
+#---------------------------------------------------------------------------- /START --> RESPOSTA1
     if ultima_mensagem == "/start":
         #MENSAGEM DE BOAS-VINDAS E ORIENTAÇÃO
         resposta = f"""
 Olá, {nome_usuario}, tudo bem?
-Eu sou o Bot Estagiário
-
 Antes de continuar, preciso que fique atento ao modo de uso da ferramenta:
 1 - Responda apenas o que for solicitado pelo bot;
 2 - AGUARDE O RETORNO PARA A DEMANDA, pois podemos demorar alguns segundos para responder;
 3 - Compreenda que sou uma ferramenta colaborativa. Mesmo após obter os resultados, será necessário revisá-los para saber se consegui atender suas expectativas;
 4 - Tenha em mãos algum link sobre a informação para que o BOT seja contextualizado com fatos dos dias atuais;
 5 - Sempre que quiser resetar a conversa, digite e envie "/start" (sem aspas);
-6 - Leia atentamente cada comando para chegar onde deseja
-7 - Esta ferramenta foi desenvolvida por Will Araújo.
-
+6 - Você pode pedir que a matéria seja refeita quantas vezes quiser, mas lembre-se de que quanto mais o assunto for detalhado e tiver links de balizamento melhor;
+7 - Esta ferramenta foi desenvolvida pelo jornalista Will Araújo.
 *************************
-
-Para continuarmos, clique no link a seguir: /menu.
+Para continuarmos, clique no link a seguir: /continuar.
 Será um prazer ajudar.
   """
 
-#---------------------------------------------------------------------------Comando /menu
+#---------------------------------------------------------------------------/CONTINUAR --> RESPOSTA2
         
-    elif ultima_mensagem == "/menu":
+    elif ultima_mensagem == "/continuar":
         #      
         #ORIENTAÇÕES PARA CONSTRUÇÃO DO ASSUNTO
         resposta = f"""
 Vamos lá. 
 
-Por favor, veja abaixo as nossas opções de trabalho.
-
-1 - /pauta para criar uma pauta a partir de um resumo e link de balizamento;
-2 - /noticia para criar uma matéria a partir de um boletim de ocorrência, pdf on-line, link de ou publicação;
-3 - /previsao para criar uma nota de previsão com base no boletim do tempo;
-4 - /carrossel para criar 5 pequenos textos com base em um link de notícias.
+Por favor, insira abaixo um assunto, um link para contextualização.
 
 
 **************
 LEMBRE-SE: links são importantes para que eu seja atualizado sobre o assunto e apresente informações mais assertivas.
 Além disso, sempre aguarde o retorno, pois a construção da pauta pode demorar até 3 minutos.
-Sempre que quiser voltar ao menu, digite ou clique em /menu
-
-
 **************
-    """
 
-#--------------------------------------------------------------------------Comando /pauta
+EXEMPLO: 
+Gostaria de obter uma pauta sobre o assunto: "XXXXXXX XXX XXXXXXXXXXXXX"
+Para balizar a abordagem e contexto, use o link: https://XXXX.XXXX.XXXX/XXXX como referência.
+A abordagem precisa ser direcionada para a editoria: ECONOMIA.
+
+OBSERVAÇÃO: quanto mais informação, mais assertiva a pauta. Por isso, seja claro sobre seus objetivos.
+    """
+    
+#---------------------------------------- INSERÇÃO DA PAUTA --> RESPOSTA3 --> RESPOSTA4 --> RESPOSTA5
 
     elif not ultima_mensagem.startswith("/") and not "@" in ultima_mensagem:
         print("É um assunto com link e chegou no CHATGPT***********")
@@ -148,18 +162,17 @@ Sempre que quiser voltar ao menu, digite ou clique em /menu
         corpo_mensagem = {
         "model": id_modelo_chatgpt,
         "messages": [{"role": "user", "content": f"""
-Olá, gostaria de trabalhar com você para que construa uma pauta jornalística. Por isso, peço que entre em um modo que familiarizado com 
+Olá, gostaria de trabalhar com você para que construa uma notícia jornalística para mim. Por isso, peço que entre em um modo que familiarizado com 
 o jornalismo brasileiro.
 Este é o assunto: {assunto}
-A pauta precisa ter o seguinte formato:
-1 - Produza uma sugestão de um título sobre o assunto com até 62 caracteres. Este tópico será chamado TÍTULO;
-2 - Produza uma introdução e contextualização a partir do link enviado. Pode ser também um resumo. Este tópico será chamado INTRODUÇÃO;
-3 - Produza uma abordagem semelhante à do link e somada a uma nova, explicando o que não foi explorado pelo texto, mas poderia ser apurado. Coloque a abordagem direciona à editoria que o usuário mencionou. Este tópico será chamado ABORDAGEM;
-4 - Sugira ao menos 3 tipos de profissionais ou profissões que podem servir de fonte para a apuração. Junto, coloque o endereço de e-mail público de cada um, caso exista. Na ausência de nomes, indique profissões ou cargos que podem servir de fontes. Este tópico será chamado FONTES DE SUGESTÃO;
-5 - Indique uma palavra-chave a ser pesquisa no Google e que pode fornecer mais links sobre o assunto. Este tópico será chamado USE ESTA PALAVRA-CHAVE E PESQUISE MAIS INFORMAÇÕES COM ELA;
-6 - Sugira ao menos cinco perguntas com base no assunto e editoria que foi enviada. Este tópico será chamado PERGUNTAS DE SUGESTÃO;
-7 - Indique quais secretarias do governo Federal, Estadual ou Municipal brasileiro que podem ajudar no assunto. Explique porque buscar essa fonte oficial é importante e qual a função dela. Este tópico será chamado FONTES OFICIAIS.
-"""
+Preciso que você faça o seguinte:
+1 - Leia o texto que está no link;
+2 - Extraia a informação do que é o fato, quem praticou o fato, onde ocorreu o fato, quando aconteceu o fato, como aconteceu o fato, por que aconteceu o fato;
+3 - Construa um texto com lide jornalística no início, no primeiro parágrafo, e depois desenvolva mais o texto a partir das informações que estão no link;
+4 - O texto precisa ter ao menos 300 palavras, mas pode ultrapassar essa quantidade
+5 - Não crie nenhuma informação nova
+6 - O texto precisa ter um tom de voz neutro
+5 -"""
            }]}
         
         #CONFIGURANDO O ENVIO DO PROMPT PARA O CHATGPT
@@ -174,17 +187,15 @@ A pauta precisa ter o seguinte formato:
         
         #CONFIGURANDO O ENVIO DA RESPOSTA DO CHATGPT PARA SER REPASSADA AO TELEGRAM
         
-        retorno_chatgpt = requisicao_chatgpt.json()
         resposta_chatgpt = None
+        retorno_chatgpt = requisicao_chatgpt.json()  
+        while not resposta_chatgpt:
             
-        while resposta_chatgpt == None:
             resposta_chatgpt = retorno_chatgpt["choices"][0]["message"]["content"]
             time.sleep(5)
-            print(f"Status code: {requisicao_chatgpt.status_code}")
-            print(f"Resposta: {requisicao_chatgpt.text}")
 
         
-        print(resposta_chatgpt)
+            print(resposta_chatgpt)
         print("Passou pelo While")        
         
         #CADASTRANDO A PAUTA NA PLANILHA
@@ -198,15 +209,44 @@ A pauta precisa ter o seguinte formato:
         #ENVIA A RESPOSTA AO TELEGRAM
         resposta = f"""{resposta_chatgpt}+
 *******************************************************
-{nome_usuario}, podemos continuar a partir dessa pauta?
+
+{nome_usuario}, O conteúdo está adequado?
+
 Clique para responder:
 1 - /Sim, vamos para a próxima etapa.
 2 - /Nao, refaça com uma abordagem diferente
 """
-        
-#----------------------------------------------------------------------------- Responde
+
+#----------------------------------------------------------------------/Sim
+    #IDENTAÇÃO
+    elif ultima_mensagem == "/Sim":
+        #
+        print("A etapa do sim deu certo e podemos continuar com e-mail")
+
+        #PEGAR O E-MAIL
+        resposta = f"""
+Tudo bem, {nome_usuario}. O conteúdo está adequado.
+Obrigado pela interação. Clique em /start para retornar para o início.
+"""
+#------------------------------------------------------------------------/Nao
+
+    elif ultima_mensagem == "/Nao":
+        print('a pauta não serviu, vamos refazer')
+
+        #MENSAGEM
+        resposta = f"""
+Tudo bem, {nome_usuario}.
+Desculpe pelo erro.
+Vamos refazer a matéria.
+
+Para isso, clique em /continuar.
+
+"""
+
+
+    
+    
     #ENVIA A MENSAGEM PARA O USUÁRIO
     novo_texto = {"chat_id": chat_id, "text": resposta}
-    requests.post(f"https://api.telegram.org/bot{token_telegram}/sendMessage", data=novo_texto)
+    requests.post(f"https://api.telegram.org./bot{token_telegram}/sendMessage", data=novo_texto)
     return "Ok"
-
